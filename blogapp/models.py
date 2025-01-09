89704f2ba3d350ff6c46a068e5bff6bf98ca537a
alphamuhia <muhiaalpha@gmail.com>
2025-01-09 20:00:22 +0300

    doen

---
+++ blogapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
